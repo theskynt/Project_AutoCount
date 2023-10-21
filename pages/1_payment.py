6627f58032c13dbf8635a7358975c1cff5dd6a33
TheSKY <92660607+theskynt@users.noreply.github.com>
2023-10-22 01:02:23 +0700

    up

--- pages/1_payment.py
+++ pages/1_payment.py
@@ -3,12 +3,69 @@ from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.stylable_container import stylable_container
 import pandas as pd
 import json
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.patches as patches
+from PIL import Image, ImageDraw
+import io
+
+
+# Load the trained model weights.
+model = YOLO("best.pt")
+
+# model.predict(source=0, show=True)
 
 camera = st.camera_input("Take a photo")
+data_in = []
+
+if camera is not None:
+    # Specify the path where you want to save the captured photo
+    photo_path = 'captured_photo.jpg'
+
+    # Save the captured photo
+    with open(photo_path, 'wb') as f:
+        f.write(camera.getvalue())
+
+    # Use the saved photo for prediction
+    results = model.predict(photo_path, conf=0.70)
+
+    photo = results[0]
+    img = mpimg.imread(photo_path)
+    fig, ax = plt.subplots(1)
+
+    # Display predictions
+    if results:
+        result = results[0]
+        img = Image.open(photo_path)
+        draw = ImageDraw.Draw(img)
+
+        for box in result.boxes:
+            class_id = result.names[box.cls[0].item()]
+            cords = box.xyxy[0].tolist()
+            cords = [round(x) for x in cords]
+            conf = round(box.conf[0].item(), 2)
+
+            # Draw a rectangle around the detected object
+            draw.rectangle(cords, outline="red", width=2)
+
+            # Add label for the object type and confidence
+            label = f"{class_id}: {conf}"
+            draw.text((cords[0], cords[1]), label, fill="red")
+
+            data_in.append(class_id)
+            # st.write("Object type:", class_id)
+            # st.write("Confidence:", conf)
+            # st.write("---")
+        
+        st.write("รูปที่เข้าโมเดลแล้ว")
+        # Display the image with bounding boxes
+        st.image(img, caption="Object Detection")
+    else:
+        st.write("No predictions found.")
+
 
 col1, col2 = st.columns([1, 1])
-
-data_in = ("Apple", "Banana", "Mango", "Apple")
 
 with open("data.json", "r") as json_file:
     data = json.load(json_file)
@@ -29,7 +86,6 @@ col1.write(
     f'<div style="font-size: 20px;">Total Price: {total_price}</div>',
     unsafe_allow_html=True
 )
-
 
 with col2:
     with stylable_container(
